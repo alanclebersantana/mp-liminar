a0e0ce3157c50e18913084d791370785bb5bb8c3
Alan <alanclebersantana@gmail.com>
2026-09-21 16:02:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mp-central-2026.09.04-1838';
+const CACHE = 'mp-central-01';
 const ASSETS = ['./','./index.html','./instalar.html','./manifest.json','./logo.png',
   './icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 
