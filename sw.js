169ead3e3bfdc1b87487be4e5771ec2dc0624e0f
Alan <alanclebersantana@gmail.com>
2026-09-22 18:51:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mp-central-03';
+const CACHE = 'mp-central-06';
 const ASSETS = ['./','./index.html','./instalar.html','./manifest.json','./logo.png',
   './icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 
